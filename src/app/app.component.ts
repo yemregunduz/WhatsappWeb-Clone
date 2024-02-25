c0de47a467669137120d50f442e57797d3445bab
yemregunduz <81508248+yemregunduz@users.noreply.github.com>
2024-02-25 15:21:00 +0300

    images added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { SidePanelComponent } from './components/side-panel/side-panel.component';
+import { MainPanelComponent } from './components/main-panel/main-panel.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,SidePanelComponent, MainPanelComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'WhatsappWeb-Clone';
+  x(){
+    console.log('x');
+  }
 }
